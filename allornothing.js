9a47bb0645c6e66340e0c91d717802942622672f
Ördög Balázs <95277305+bazsazs@users.noreply.github.com>
2024-01-16 09:24:06 +0100

    Update allornothing.js

--- allornothing.js
+++ allornothing.js
@@ -1,11 +1,11 @@
 function canScore100(answerKey, studentAnswers) { 
     for (let i = 0; i < answerKey.length; i++) { 
-        if (answerKey[i] == '_' && answerKey[i] !== studentAnswers[i]) { 
+        if (answerKey[i] !== '_' && answerKey[i] !== studentAnswers[i]) { 
             return false; } } 
             return true; 
         } 
 
-const answerKey = ['A', 'B', '_', 'C']; 
-const studentAnswers = ['A', 'B', 'D', 'C']; 
+const answerKey = ['A', 'B', 'D', 'C']; 
+const studentAnswers = ['A', 'B', '_', 'C']; 
 const result = canScore100(answerKey, studentAnswers); 
 console.log(result);
